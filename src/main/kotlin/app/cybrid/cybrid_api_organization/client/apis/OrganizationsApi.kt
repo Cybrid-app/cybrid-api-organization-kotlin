b894ff9b602a645e1fef0db43b0d7922efa68a9d
brent-cybrid <brent@cybrid.app>
2022-04-28 12:52:48 +0000

    v0.1.11

---
+++ src/main/kotlin/app/cybrid/cybrid_api_organization/client/apis/OrganizationsApi.kt
@@ -0,0 +1,37 @@
+package app.cybrid.cybrid_api_organization.client.apis
+
+import app.cybrid.cybrid_api_organization.client.infrastructure.CollectionFormats.*
+import retrofit2.http.*
+import retrofit2.Response
+import okhttp3.RequestBody
+
+import app.cybrid.cybrid_api_organization.client.models.OrganizationOrganizationModel
+import app.cybrid.cybrid_api_organization.client.models.PatchOrganizationOrganizationModel
+
+interface OrganizationsApi {
+    /**
+     * Get organization
+     * Retrieve an organization.  Required scope: **organizations:read**
+     * Responses:
+     *  - 200: organization found
+     *
+     * @param organizationGuid Identifier for the organization. 
+     * @return [OrganizationOrganizationModel]
+     */
+    @GET("api/organizations/{organization_guid}")
+    suspend fun getOrganization(@Path("organization_guid") organizationGuid: kotlin.String): Response<OrganizationOrganizationModel>
+
+    /**
+     * Patch organization
+     * Update an organization.  Required scope: **organizations:write**
+     * Responses:
+     *  - 200: organization found
+     *
+     * @param organizationGuid Identifier for the organization. 
+     * @param patchOrganizationOrganizationModel  
+     * @return [OrganizationOrganizationModel]
+     */
+    @PATCH("api/organizations/{organization_guid}")
+    suspend fun updateOrganization(@Path("organization_guid") organizationGuid: kotlin.String, @Body patchOrganizationOrganizationModel: PatchOrganizationOrganizationModel): Response<OrganizationOrganizationModel>
+
+}
